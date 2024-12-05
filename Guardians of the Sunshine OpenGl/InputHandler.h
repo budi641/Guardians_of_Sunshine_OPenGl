270318dc8db348b2ca1b389b021183e1f461a223
R00V <rofikhaled.2004@gmail.com>
2024-12-06 01:19:36 +0200

    input-sys with glfw instead of sdl

--- Guardians of the Sunshine OpenGl/InputHandler.h
+++ Guardians of the Sunshine OpenGl/InputHandler.h
@@ -1,32 +1,35 @@
-#pragma once
 
-#include <SDL2/SDL.h>
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
 #include "Camera.h"
-
-struct InputState {
-    bool forward = false;
-    bool back = false;
-    bool left = false;
-    bool right = false;
-    bool up = false;
-    bool down = false;
-    bool sprint = false;
-    bool zoom_in = false;
-    bool  zoom_out = false;
-};
 
 class InputHandler {
 
 
+
 private:
+    struct InputState {
+        bool forward = false;
+        bool back = false;
+        bool left = false;
+        bool right = false;
+        bool up = false;
+        bool down = false;
+        bool sprint = false;
+        bool zoom_in = false;
+        bool zoom_out = false;
+    };
+
     InputState inputState;
-    
-
+    float sprint_val = 1.0f;
 public:
-    InputHandler();
-    void handleInput(SDL_Event& event);  
-    void updateCameraMovement(Camera& camera, float deltaTime); 
-    void updateCameraRotation(Camera& camera); 
 
+    InputHandler();
+    void handleInput(GLFWwindow* window);
+
+    void updateCameraMovement(Camera& camera, float deltaTime);
+    void updateCameraRotation(GLFWwindow* window, Camera& camera);
 
 };
+
+
